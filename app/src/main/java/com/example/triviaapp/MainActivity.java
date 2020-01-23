e9cb6e317e282d3f0af0dbd431a85ad7d15036e3
Akanshu Kumar Singh <singhakanshu7@gmail.com>
2020-01-23 19:32:37 +0530

    DevelopementStage1

---
+++ app/src/main/java/com/example/triviaapp/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.triviaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.android.volley.toolbox.Volley;
+
+public class MainActivity extends AppCompatActivity {
+    Volley volley;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+    }
+}
