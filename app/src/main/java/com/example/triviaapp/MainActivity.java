ba83096808b06f0f1f7dfd0995f3ac3762508af4
Akanshu Kumar Singh <singhakanshu7@gmail.com>
2020-01-25 18:08:06 +0530

    Trivia Completed

--- app/src/main/java/com/example/triviaapp/MainActivity.java
+++ app/src/main/java/com/example/triviaapp/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.triviaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.animation.ValueAnimator;
+import android.graphics.Color;
+import android.graphics.drawable.Animatable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Switch;
@@ -20,7 +26,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
+    ValueAnimator anim;
     private TextView counter;
     private TextView quesTextView;
     private Button trueButton;
@@ -30,7 +36,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private int currentIndex = 0;
     private List<Question> questionList;
     private boolean answer;
-
+    private CardView cardView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,7 +48,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         falseButton = findViewById(R.id.falseButton);
         quesTextView = findViewById(R.id.quesTextView);
         counter = findViewById(R.id.counter);
-
+        cardView = findViewById(R.id.cardView);
+        quesTextView.setBackgroundColor(getResources().getColor(R.color.back));
         questionList = new QuestionBank().getQuestion(new AnswerListAsyncResponse() {
             @Override
             public void processFinished(ArrayList<Question> questionArrayList) {
@@ -86,24 +93,66 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.prevButton:
+                anim.end();
+                quesTextView.setBackgroundColor(getResources().getColor(R.color.back));
                 updateQuestion(prevButton);
                 break;
             case R.id.nextButton:
+                anim.end();
+                quesTextView.setBackgroundColor(getResources().getColor(R.color.back));
                 updateQuestion(nextButton);
+
                 break;
             case R.id.trueButton:
                 if (answer == true) {
                     Toast.makeText(MainActivity.this, "Correct Answer :) ", Toast.LENGTH_SHORT).show();
-                } else
+                    shakeAnimation();
+                    } else {
                     Toast.makeText(MainActivity.this, "InCorrect Answer :( ", Toast.LENGTH_SHORT).show();
+                    shakeAnimation1();
+                }
                 break;
             case R.id.falseButton:
                 if (answer == false) {
                     Toast.makeText(MainActivity.this, "Correct Answer :) ", Toast.LENGTH_SHORT).show();
-                } else
+                    shakeAnimation();
+                    } else {
                     Toast.makeText(MainActivity.this, "InCorrect Answer :( ", Toast.LENGTH_SHORT).show();
+                }
                 break;
         }
+    }
+    void shakeAnimation(){
+        anim = ValueAnimator.ofFloat(0, 3);
+        anim.setDuration(500);
+
+        final float[] hsv;
+        final int[] runColor = new int[1];
+        int hue = 0;
+        hsv = new float[3]; // Transition color
+        hsv[1] = 1;
+        hsv[2] = 1;
+        anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+
+                hsv[0] = 360 * animation.getAnimatedFraction();
+
+                runColor[0] = Color.HSVToColor(hsv);
+                quesTextView.setBackgroundColor(runColor[0]);
+            }
+        });
+
+        anim.setRepeatCount(30);
+        anim.start();
+    }
+
+    void shakeAnimation1(){
+        CardView cardView = findViewById(R.id.cardView);
+        quesTextView.setBackgroundColor(getResources().getColor(R.color.wrong));
+        cardView.animate().setDuration(700).rotationBy(360);
 
     }
+
 }
